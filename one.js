9cdb15989922b142a76ce7a38bac47a108a3cb09
jrtorresb <37489423+jrtorresb@users.noreply.github.com>
2018-08-02 12:00:07 -0500

    Create one.js

---
+++ one.js
@@ -0,0 +1,62 @@
+console.log(1+2);
+
+// var camelCase
+// RUBY : snake_case
+
+/**
+ * Tipo de datos:
+Números
+Booleanos
+Objeto, HASH O DICCIONARIO, ARRAY O LISTA
+
+Variables
+
+var firstName;
+ * 
+**/
+
+var firstName="Ben";
+var lastName=" Delacre";
+var fullName=firstName+lastName;
+
+console.log(firstName);
+console.log(lastName);
+console.log(fullName);
+console.log(firstName+lastName);
+
+
+var meteo=prompt("Ingresa");
+// var meteo="sol";
+
+if (meteo=="sol")
+{
+  console.log("No llueve");
+}
+else if (meteo=="llueve")
+{
+  console.log("llueve tapate");
+}
+else
+{
+  console.log("No pasa nada");
+}
+
+// logicos
+
+/*
+ 
+ var result = a&&b;  y lógico
+ var result =!a;
+
+ */
+
+var Bob="Bob";
+var Ben="Ben";
+var Pat="Pat";
+
+var bNombres="";
+
+
+
+
+console.log(bNombres)
